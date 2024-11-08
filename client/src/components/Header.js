da1fef42666ca40b93a8c0619e179d553a290ffc
EC2 Default User <ec2-user@ip-10-1-3-244.ec2.internal>
2024-11-08 22:33:14 +0000

    V 13

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "MEDDIFLUX CICD 2024 V12",
+  title: "MEDDIFLUX CICD 2024 V13",
 };
 
 export default Header;
